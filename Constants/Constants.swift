dbe1aaef7a9542853da9de944e457cdea922b5c3
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-08-28 09:44:27 -0700

    Updating Constants.swift with the key constant

--- Constants/Constants.swift
+++ Constants/Constants.swift
@@ -1,1 +1,15 @@
+//
+//  Constants.swift
+//  Ask ChatGPT iOS
+//
+//  Created by Farial Mahmod on 8/24/23.
+//
+
+import Foundation
+
+enum Constants {
+// get the free key from OpenAI's API doc and interpolate the 'openAIAPIKey' constant
+static let openAIAPIKey = ""
+}
+
 
